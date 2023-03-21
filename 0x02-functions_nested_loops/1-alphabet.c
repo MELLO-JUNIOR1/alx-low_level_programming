b5908c3cdb80fee102998b3bdb5a20fa10b58f06
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-03-21 16:04:49 -0700

    task 1Write a function that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet - prints all alphabets
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
