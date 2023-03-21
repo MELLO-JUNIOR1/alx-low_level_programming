a2de69fb8c834983a8eb98cf7ad0353448bb3fa1
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-03-21 16:49:52 -0700

    task 8 I'm federal agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: no return
+ */
+
+void jack_bauer(void)
+{
+
+int h, m;
+
+for (h = 0; h <= 23; h++)
+{
+
+for (m = 0; m < 60; m++)
+{
+
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+
+}
+
+}
+}
