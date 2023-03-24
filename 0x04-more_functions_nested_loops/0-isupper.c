982722a238c346d14cc9e3990a0679dc8b8f6395
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-03-23 17:01:06 -0700

    task 0 isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - Prints last digit
+ *
+ * @c: intiger input number
+ *
+ * Return: last digit.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+#C - More functions, more nested loops
+~~~~
+C - More functions, more nested loops first readme file
+~~~~
