c24aad4813a212fc8f7bd40d45b908336b158d5f
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-03-23 17:07:14 -0700

    task 2 Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Prints the digit
+ *
+ * @a: input number as an integer.
+ * @b: intiger input number
+ *
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
