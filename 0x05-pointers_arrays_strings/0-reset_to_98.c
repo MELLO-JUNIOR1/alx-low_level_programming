f1ed48709a545692f3bac250a78b24f562612cc9
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-03-27 17:28:35 -0700

    task 0 98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - function to reset value of n yo 98
+ * main - Entry function
+ * @n: pointer
+ */
+
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+#C - Pointers, arrays and strings
+~~~~
+C - Pointers, arrays and strings first readme file
+~~~~
