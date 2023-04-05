d626197bdfbecb34434c9574ff7c8a834bc2d925
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-04-05 16:50:25 -0700

    Push the first task files to github :)

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string.
+ * @s: pointer to string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,4 @@
+#C - Recursion
+~~~~
+C - Recursion first readme file
+~~~~
