22cf155e8a2a26e69d8d742903bffe1d5cad0fb4
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-04-13 17:51:40 -0700

    hot fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,52 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * _atoi - convert string to an integer
  * @str: string
+ *
  * Return: integer
  */
-char _atoi(char *str)
+int _atoi(char *str)
 {
 	int sign = 1;
-	unsigned long int dig = 0, num1, i;
+	unsigned int dig = 0, i;
 
-	for (num1 = 0; !(str[num1] <= 48 && str[num1] >= 57); num1++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[num1] == '-')
+		if (str[i] == '-')
 			sign *= -1;
-	}
-
-	for (i = num1; str[i] >= 48 && str[i] <= 57; i++)
-	{
-		dig *= 10;
-		dig += (str[i] - 48);
+		else if (str[i] >= '0' && str[i] <= '9')
+			dig = dig * 10 + (str[i] - '0');
+		else
+		{
+			printf("Error\n");
+			exit(98);
+		}
 	}
 
 	return (sign * dig);
 }
-/**
- * print_num - prints number
- * @num: number
- * Return: void
- */
-void print_num(unsigned long int num)
-{
-	unsigned long int div = 1, i, rem;
 
-	for (i = 0; num / div > 9; i++, div %= 10)
-		;
-
-	for (; div >= 1; num %= div, div /= 10)
-	{
-		rem = num / div;
-		putchar(rem + '0');
-	}
-}
 /**
  * main - prints multiplication result, followed by newline
  * @argc: count of args
  * @argv: pointer to array of args
+ *
  * Return: multiplication result
  */
 int main(int argc, char *argv[])
@@ -57,7 +44,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	print_num(_atoi(argv[1]) * _atoi(argv[2]));
+
+	int num1 = _atoi(argv[1]);
+	int num2 = _atoi(argv[2]);
+
+	unsigned long int result = (unsigned long int)num1 * (unsigned long int)num2;
+
+	print_num(result);
 	putchar('\n');
 
 	return (0);
