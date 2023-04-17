4e2048514a7b3aa4b180f324fc88392b480cfe1c
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-04-17 16:47:29 -0700

    task 0 poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+#0x0E. C - Structures, typedef
+~~~~
+0x0E. C - Structures, typedef first readme file
+~~~~
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog infos
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
