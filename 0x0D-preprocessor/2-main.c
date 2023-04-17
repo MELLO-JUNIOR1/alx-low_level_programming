ba20558971bc90adc100a9ef5125ae3984045a71
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-04-17 15:34:59 -0700

    Push all the files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: file name
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+return (0);
+}
