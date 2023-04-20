ee0040ca814c5e6bdf814319a3d14913616802e7
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-04-19 17:34:00 -0700

    Push the first task files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: string
+ * @f: pointer function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+#0x0F. C - Function pointers
+~~~~
+0x0F. C - Function pointers first readme file
+~~~~
