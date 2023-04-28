6ff3938948c93acff44c45671e5395faf5d2d082
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-04-27 18:37:00 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - Function that prints the number of ellements
+ * @h: pointer
+ * Return: Return n
+ */
+
+size_t list_len(const list_t *h)
+{
+unsigned int count = 0;
+
+while (h)
+{
+count++;
+		h = h->next;
+}
+
+
+
+return (n);
+}
