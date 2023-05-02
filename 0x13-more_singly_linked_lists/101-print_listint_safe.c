2b398d546c3616807b6c6cc87c16d13016bc63ab
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-05-02 12:29:11 -0700

    101 hot fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,29 @@
 #include "lists.h"
-
 /**
- * print_listint_safe - Function prints a list
- * @head: pointer
- * Return: Return n
+ * print_listint_safe - Prints a listint_t list
+ * @head: pointer to the head of head
+ * Return: number of heads list || 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
-int n = 0;
-const listint_t *p;
-const listint_t *not;
+size_t count = 0;
 
-p = head;
-not = head;
-
-if (head == NULL)
+if (!head)
 exit(98);
 
-while (not_n)
+while (head != NULL)
 {
-printf("[%p] %d\n", (void *)p, p->n);
-n++;
+printf("[%p] %d\n", (void *)head, head->n);
+count++;
 
-if (not <= not->next)
+if (head->next >= head)
 {
-printf("-> [%p] %d\n", (void *)not->next, not->next->n);
+head = head->next;
+printf("-> [%p] %d\n", (void *)head, head->n);
 break;
 }
-p = p->next;
-not = not->next;
+head = head->next;
 }
 
-if (not)
-{
-exit(98);
-}
-
-return (n);
+return (count);
 }
