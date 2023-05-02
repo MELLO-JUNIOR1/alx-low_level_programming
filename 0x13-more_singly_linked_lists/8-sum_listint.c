bafc3ec1a3987e9c14be97bbe69481555a8bc84b
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-05-02 12:23:12 -0700

    another hot fix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,15 +7,15 @@
  */
 int sum_listint(listint_t *head)
 {
-int s = 0;
+int sum = 0;
     
 if (head == NULL)
 return (0);
 while (head != NULL)
 {
-s += head->n;
+sum += head->n;
 head = head->next;
 }
     
-return (s);
+return (sum);
 }
