986829416d5524a5cef769329cb344d53b441a96
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-05-04 17:10:17 -0700

    sec file1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,37 +2,12 @@
 
 /**
  * print_binary - Prints the binary representation of a number
- * @n: number
- * Return: void
+ * @n: number to convert to binary and print
  */
+
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-	int found_one = 0;
-
-	while (mask)
-	{
-		if (n & mask)
-		{
-			found_one = 1;
-			_putchar('1');
-		}
-		else if (found_one)
-		{
-			_putchar('0');
-		}
-
-		mask >>= 1;
-	}
-
-	if (!found_one)
-	{
-		_putchar('0');
-	}
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
