e973d0ab7bf32ac3db415324821c542f7943a813
Hamza Bouzakarya <marshmello1ayt@gmail.com>
2023-06-15 17:48:47 -0700

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,60 +1,51 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint_end - adds a new node at the end
- * @head: head of double list
- * @n: new node
- * Return: the address of the new element, or NULL if it failed
+ * *insert_dnodeint_at_index - Function that add node at an index
+ * @idx: index
+ * @n: data
+ * @h: pointer
+ * Return: Return new
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-/**
- * add_dnodeint -  adds a new node at the beginning
- * @head: head of double list
- * @n: new node
- * Return: the address of the new element, or NULL if it failed
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-/**
- * insert_dnodeint_at_index -  inserts a new
- * node at a given position
- * @h: head of double list
- * @idx: index where to insert
- * @n: value to insert
- * Return: address of the new node
- * or NULL if it failed
- */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *nav, *new, *before;
+	dlistint_t *crr = *h;
+	dlistint_t *nx;
+	dlistint_t *new;
 	unsigned int i = 0;
 
-	if (!h)
-		return (NULL);
-	nav = *h;
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	while (nav)
-	{
-		if (i == idx - 1)
-			before = nav;
 
-		if (i == idx)
-		{
-			new->next = nav;
-			nav->prev = new;
-			before->next = new;
-			new->prev = before;
-			return (new);
-		}
-		if (!nav->next && i + 1 == idx)
-			return (add_dnodeint_end(h, n));
-		nav = nav->next;
-		i++;
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new;
+		*h = new;
+		return (new);
 	}
-	return (NULL);
+
+	for (i = 0; crr != NULL && i < idx - 1; i++)
+		crr = crr->next;
+
+	if (!crr)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	nx = crr->next;
+	new->prev = crr;
+	new->next = nx;
+	crr->next = new;
+	if (nx != NULL)
+		nx->prev = new;
+return (new);
 }
